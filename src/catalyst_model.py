549d74cddefcc649bb76358d95ef2353b8692e4a
joshvarty@gmail.com <joshvarty@gmail.com>
2019-10-31 15:08:21 -0400

    Adjust threshold for ReduceLR

--- src/catalyst_model.py
+++ src/catalyst_model.py
@@ -1,6 +1,7 @@
 import os
 import cv2
 import tqdm
+import multiprocessing
 from multiprocessing import Process
 
 
@@ -15,6 +16,7 @@ from torch.utils.data import TensorDataset, DataLoader, Dataset
 from torch.optim.lr_scheduler import StepLR, ReduceLROnPlateau, CosineAnnealingLR
 from torchvision.transforms import ToTensor
 
+import catalyst.utils as utils
 from catalyst.dl.runner import SupervisedRunner
 from catalyst.dl.callbacks import DiceCallback, EarlyStoppingCallback, InferCallback, CheckpointCallback
 
@@ -89,7 +91,8 @@ def get_training_augmentation():
         albu.ShiftScaleRotate(scale_limit=(0.1, 0.1), rotate_limit=45, p=0.5, border_mode=0),
         albu.GridDistortion(p=0.5),
         albu.RandomContrast(limit=0.3, p=0.5),
-        albu.RandomCrop(512, 512)
+        albu.Resize(704, 1056),
+        albu.RandomCrop(704, 704)
     ]
 
     return albu.Compose(train_transform)
@@ -98,7 +101,7 @@ def get_training_augmentation():
 def get_validation_augmentation():
     """Add paddings to make image shape divisible by 32"""
     test_transform = [
-        albu.Resize(1408, 2112)  # Validate/test on full size images
+        albu.Resize(704, 1056)  # Validate/test on full size images
     ]
     return albu.Compose(test_transform)
 
@@ -111,7 +114,7 @@ def get_preprocessing(preprocessing_function):
     """Construct preprocessing transform
 
     Args:
-        preprocessing_fn (callbale): data normalization function
+        preprocessing_function (callbale): data normalization function
             (can be specific for each pretrained neural network)
     Return:
         transform: albumentations.Compose
@@ -207,7 +210,6 @@ class CloudDataset(Dataset):
     def __getitem__(self, idx):
 
         img = self.samples[idx]
-        image_name = self.img_ids[idx]
 
         if self.datatype != 'test':
             encoded_masks = self.raw_masks[idx]
@@ -230,27 +232,28 @@ class CloudDataset(Dataset):
     def _clear(self):
         del self.samples[:]
 
+train = pd.read_csv(f'data/train.csv')
+sub = pd.read_csv(f'data/sample_submission.csv')
 
-def train():
-    train = pd.read_csv(f'data/train.csv')
-    sub = pd.read_csv(f'data/sample_submission.csv')
+train['label'] = train['Image_Label'].apply(lambda x: x.split('_')[1])
+train['im_id'] = train['Image_Label'].apply(lambda x: x.split('_')[0])
 
-    train['label'] = train['Image_Label'].apply(lambda x: x.split('_')[1])
-    train['im_id'] = train['Image_Label'].apply(lambda x: x.split('_')[0])
+sub['label'] = sub['Image_Label'].apply(lambda x: x.split('_')[1])
+sub['im_id'] = sub['Image_Label'].apply(lambda x: x.split('_')[0])
 
-    sub['label'] = sub['Image_Label'].apply(lambda x: x.split('_')[1])
-    sub['im_id'] = sub['Image_Label'].apply(lambda x: x.split('_')[0])
+id_mask_count = train.loc[train['EncodedPixels'].isnull() == False, 'Image_Label'].apply(
+    lambda x: x.split('_')[0]).value_counts().reset_index().rename(columns={'index': 'img_id', 'Image_Label': 'count'})
+train_ids, valid_ids = train_test_split(id_mask_count['img_id'].values, random_state=42, stratify=id_mask_count['count'], test_size=0.1)
+test_ids = sub['Image_Label'].apply(lambda x: x.split('_')[0]).drop_duplicates().values
+logdir = "./logs/segmentation"
+ENCODER = 'resnet50'
+ENCODER_WEIGHTS = 'imagenet'
+DEVICE = 'cuda'
+ACTIVATION = None
 
-    id_mask_count = train.loc[train['EncodedPixels'].isnull() == False, 'Image_Label'].apply(
-        lambda x: x.split('_')[0]).value_counts().reset_index().rename(columns={'index': 'img_id', 'Image_Label': 'count'})
-    train_ids, valid_ids = train_test_split(id_mask_count['img_id'].values, random_state=42, stratify=id_mask_count['count'], test_size=0.1)
-    test_ids = sub['Image_Label'].apply(lambda x: x.split('_')[0]).drop_duplicates().values
 
-    ENCODER = 'resnet50'
-    ENCODER_WEIGHTS = 'imagenet'
-    DEVICE = 'cuda'
+def train_model():
 
-    ACTIVATION = None
     model = smp.Unet(
         encoder_name=ENCODER,
         encoder_weights=ENCODER_WEIGHTS,
@@ -260,7 +263,7 @@ def train():
     preprocessing_fn = smp.encoders.get_preprocessing_fn(ENCODER, ENCODER_WEIGHTS)
 
     num_workers = 0
-    bs = 8
+    bs = 6
     train_dataset = CloudDataset(df=train, datatype='train', img_ids=train_ids, transforms=get_training_augmentation(), preprocessing=get_preprocessing(preprocessing_fn))
     valid_dataset = CloudDataset(df=train, datatype='valid', img_ids=valid_ids, transforms=get_validation_augmentation(), preprocessing=get_preprocessing(preprocessing_fn))
 
@@ -272,16 +275,16 @@ def train():
         "valid": valid_loader
     }
 
-    num_epochs = 1
-    logdir = "./logs/segmentation"
+    num_epochs = 25
 
     # model, criterion, optimizer
     optimizer = RAdam([
         {'params': model.decoder.parameters(), 'lr': 1e-2},
         {'params': model.encoder.parameters(), 'lr': 1e-3},
     ])
-    scheduler = ReduceLROnPlateau(optimizer, factor=0.15, patience=2)
+    scheduler = ReduceLROnPlateau(optimizer, factor=0.15, patience=2, threshold=0.001)
     criterion = smp.utils.losses.BCEDiceLoss(eps=1.)
+
     runner = SupervisedRunner()
 
     runner.train(
@@ -299,18 +302,154 @@ def train():
     return True
 
 
-# multiprocessing_pool = multiprocessing.Pool(1)
-# result = multiprocessing_pool.map(train, [])
-#
-# multiprocessing_pool.terminate()
-# multiprocessing_pool.join()
+def generate_class_params(i_dont_know_how_to_return_values_without_map):
 
-p = Process(target=train)
+    preprocessing_fn = smp.encoders.get_preprocessing_fn(ENCODER, ENCODER_WEIGHTS)
+    valid_dataset = CloudDataset(df=train, datatype='valid', img_ids=valid_ids, transforms=get_validation_augmentation(), preprocessing=get_preprocessing(preprocessing_fn))
+    valid_loader = DataLoader(valid_dataset, batch_size=1, shuffle=False, num_workers=0)
+
+    model = smp.Unet(
+        encoder_name=ENCODER,
+        encoder_weights=ENCODER_WEIGHTS,
+        classes=4,
+        activation=ACTIVATION,
+    )
+
+    runner = SupervisedRunner()
+    # Generate validation predictions
+    encoded_pixels = []
+    loaders = {"infer": valid_loader}
+    runner.infer(
+        model=model,
+        loaders=loaders,
+        callbacks=[
+            CheckpointCallback(
+                resume=f"{logdir}/checkpoints/best.pth"),
+            InferCallback()
+        ],
+    )
+
+    valid_masks = []
+    probabilities = np.zeros((2220, 350, 525))
+    for i, (batch, output) in enumerate(tqdm.tqdm(zip(
+            valid_dataset, runner.callbacks[0].predictions["logits"]))):
+        image, mask = batch
+        for m in mask:
+            if m.shape != (350, 525):
+                m = cv2.resize(m, dsize=(525, 350), interpolation=cv2.INTER_LINEAR)
+            valid_masks.append(m)
+
+        for j, probability in enumerate(output):
+            if probability.shape != (350, 525):
+                probability = cv2.resize(probability, dsize=(525, 350), interpolation=cv2.INTER_LINEAR)
+            probabilities[i * 4 + j, :, :] = probability
+
+
+    class_params = {}
+    for class_id in range(4):
+        print(class_id)
+        attempts = []
+        for t in range(30, 100, 5):
+            t /= 100
+            for ms in [1200, 5000, 10000]:
+                masks = []
+                for i in range(class_id, len(probabilities), 4):
+                    probability = probabilities[i]
+                    predict, num_predict = post_process(sigmoid(probability), t, ms)
+                    masks.append(predict)
+
+                d = []
+                for i, j in zip(masks, valid_masks[class_id::4]):
+                    if (i.sum() == 0) & (j.sum() == 0):
+                        d.append(1)
+                    else:
+                        d.append(dice(i, j))
+
+                attempts.append((t, ms, np.mean(d)))
+
+        attempts_df = pd.DataFrame(attempts, columns=['threshold', 'size', 'dice'])
+
+        attempts_df = attempts_df.sort_values('dice', ascending=False)
+        print(attempts_df.head())
+        best_threshold = attempts_df['threshold'].values[0]
+        best_size = attempts_df['size'].values[0]
+
+        class_params[class_id] = (best_threshold, best_size)
+
+    return class_params
+
+
+def generate_test_preds(class_params):
+
+    preprocessing_fn = smp.encoders.get_preprocessing_fn(ENCODER, ENCODER_WEIGHTS)
+    dummy_dataset = CloudDataset(df=sub, datatype='test', img_ids=test_ids[:1],  transforms=get_validation_augmentation(),
+                                preprocessing=get_preprocessing(preprocessing_fn))
+    dummy_loader = DataLoader(dummy_dataset, batch_size=1, shuffle=False, num_workers=0)
+
+    model = smp.Unet(
+        encoder_name=ENCODER,
+        encoder_weights=ENCODER_WEIGHTS,
+        classes=4,
+        activation=ACTIVATION,
+    )
+    runner = SupervisedRunner(model)
+
+    # HACK: We are loading a few examples from our dummy loader so catalyst will properly load the weights
+    # from our checkpoint
+    loaders = {"test": dummy_loader}
+    runner.infer(
+        model=model,
+        loaders=loaders,
+        callbacks=[
+            CheckpointCallback(
+                resume=f"{logdir}/checkpoints/best.pth"),
+            InferCallback()
+        ],
+    )
+
+    # Now we do real inference on the full dataset
+    test_dataset = CloudDataset(df=sub, datatype='test', img_ids=test_ids,  transforms=get_validation_augmentation(),
+                                preprocessing=get_preprocessing(preprocessing_fn))
+    test_loader = DataLoader(test_dataset, batch_size=1, shuffle=False, num_workers=0)
+
+    encoded_pixels = []
+    image_id = 0
+    for i, test_batch in enumerate(tqdm.tqdm(test_loader)):
+        runner_out = runner.predict_batch({"features": test_batch[0].cuda()})['logits'].cpu().detach().numpy()
+        for i, batch in enumerate(runner_out):
+            for probability in batch:
+
+                # probability = probability.cpu().detach().numpy()
+                if probability.shape != (350, 525):
+                    probability = cv2.resize(probability, dsize=(525, 350), interpolation=cv2.INTER_LINEAR)
+                predict, num_predict = post_process(sigmoid(probability), class_params[image_id % 4][0],
+                                                    class_params[image_id % 4][1])
+                if num_predict == 0:
+                    encoded_pixels.append('')
+                else:
+                    r = mask2rle(predict)
+                    encoded_pixels.append(r)
+                image_id += 1
+
+    print("Saving submission...")
+    sub['EncodedPixels'] = encoded_pixels
+    sub.to_csv('submission.csv', columns=['Image_Label', 'EncodedPixels'], index=False)
+    print("Saved.")
+
+# Train model
+p = Process(target=train_model)
 p.start()
 p.join()
 
-myVariable = 5
+# Get valid preds and optimize threshold and min_size
+class_params = {}
+with multiprocessing.Pool(1) as p:
+    class_params = p.map(generate_class_params, [1])[0]
+    print("Got back", class_params)
 
-xx = np.random.randn(2000,3,350,525)
+# Generate test predictions
+with multiprocessing.Pool(1) as p:
+    result = p.map(generate_test_preds, [class_params])
+    print(result)
 
-myVariable = 6
+print("done")
